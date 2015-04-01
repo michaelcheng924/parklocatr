59156e4316a28a8c8ea1cc515bca9db4c16247ba
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-31 22:52:15 -0500

    move server.js to root directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('../client'));
+app.use(express.static(__dirname + '/client'));
 
 var port = process.env.PORT || 3000;
 
