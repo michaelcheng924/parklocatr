4770272c9205c65e1898ca9286f049b3c5bee00a
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-31 23:04:04 -0500

    edit Park.js

--- client/scripts/models/park.js
+++ client/scripts/models/park.js
@@ -13,15 +13,3 @@ var Park = Backbone.Model.extend({
     'Y_Coordinate': ''
   }
 });
-
-/*"Name" : "BEVERLY HILLS PARK",
-        "Address" : "10111 Kingspoint",
-        "CityMuni" : "Houston",
-        "County" : "Harris",
-        "State" : "TX",
-        "Zip" : "77075",
-        "Park_Acres" : 22.815370770000001,
-        "Park_Type" : "Community Park, Community Center ",
-        "Flag" : "M",
-        "X_Coordinate" : 3163987.8204160999,
-        "Y_Coordinate" : 13790383.07900334*/
