813b420a254bbb9c4f9fbe5a04b4070f2f8fe802
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-11 21:51:15 -0500

    change to places radar search and add icon

--- client/scripts/views/ParkView.js
+++ client/scripts/views/ParkView.js
@@ -3,7 +3,7 @@ var ParkView = Backbone.View.extend({
   tagName: 'li',
 
   initialize: function() {
-    this.template = _.template('<strong>Name: </strong><%- Name %>, <strong>X-Coordinate: </strong><%- X_Coordinate %>, <strong>Y-Coordinate: </strong><%- Y_Coordinate %>');
+    this.template = _.template('<strong>lat: </strong><%- geometry.location.lat %>, <strong>lng: </strong> <%- geometry.location.lng %>, <strong>place_id: </strong><%- place_id %>');
   },
 
   events: {
@@ -11,6 +11,6 @@ var ParkView = Backbone.View.extend({
   },
 
   render: function() {
-    return this.$el.html(this.template(this.model.attributes.attributes));
+    return this.$el.html(this.template(this.model.attributes));
   }
 });
