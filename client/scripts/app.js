8e1db72a371b55c0000830686cad2bd24621e801
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-12 20:43:42 -0500

    move script to app.js

---
+++ client/scripts/app.js
@@ -0,0 +1,169 @@
+var map;
+    var infoWindow;
+    var service;
+// ================= AUTO-COMPLETE ================= //
+    function initialize() {
+      var mapOptions = {
+        center: new google.maps.LatLng(37.6, -95.665),
+        zoom: 4
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+      infoWindow = new google.maps.InfoWindow();
+      service = new google.maps.places.PlacesService(map);
+
+      var input = /** @type {HTMLInputElement} */(
+          document.getElementById('pac-input'));
+
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+      });
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        map.setCenter(place.geometry.location);
+        map.setZoom(13);
+
+        marker.setIcon(/** @type {google.maps.Icon} */({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+
+        console.log(place.geometry.location);
+
+        google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+      });
+
+      // Sets a listener on a radio button to change the filter type on Places
+      // Autocomplete.
+      function setupClickListener(id, types) {
+        var radioButton = document.getElementById(id);
+        google.maps.event.addDomListener(radioButton, 'click', function() {
+          autocomplete.setTypes(types);
+        });
+      }
+
+      // setupClickListener('changetype-all', []);
+      // setupClickListener('changetype-address', ['address']);
+      // setupClickListener('changetype-establishment', ['establishment']);
+      // setupClickListener('changetype-geocode', ['geocode']);
+    }
+
+// ======== RADAR SEARCH WITH DETAILS ========= //
+    // var map;
+    // var infoWindow;
+    // var service;
+
+    // function initialize() {
+    //   map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //     center: new google.maps.LatLng(29.7632836, -95.3632715),
+    //     zoom: 13,
+    //     styles: [
+    //       {
+    //         stylers: [
+    //           { visibility: 'simplified' }
+    //         ]
+    //       },
+    //       {
+    //         elementType: 'labels',
+    //         stylers: [
+    //           { visibility: 'off' }
+    //         ]
+    //       }
+    //     ]
+    //   });
+
+    //   infoWindow = new google.maps.InfoWindow();
+    //   service = new google.maps.places.PlacesService(map);
+
+    //   google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+    // }
+
+    function performSearch() {
+      var request = {
+        bounds: map.getBounds(),
+        types: ['park']
+      };
+      service.radarSearch(request, callback);
+    }
+
+    function callback(results, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+      for (var i = 0, result; result = results[i]; i++) {
+        createMarker(result);
+        // service.getDetails(result, function(parkDetails, status) {
+        //   console.log(parkDetails);
+        // });
+      }
+    }
+
+    function createMarker(place) {
+      var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: 'images/park-icon.png',
+        position: place.geometry.location,
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        toggleBounce();
+        service.getDetails(place, function(result, status) {
+          if (status != google.maps.places.PlacesServiceStatus.OK) {
+            alert(status);
+            return;
+          }
+          var name = '<strong>' + result.name + '</strong>';
+          var address = '<div>' + result.address_components[0].long_name + ' ' + result.address_components[1].long_name + '<br>' + result.address_components[2].long_name + ', ' + result.address_components[3].short_name + ' ' + result.address_components[5].long_name + '</div>';
+          
+          infoWindow.setContent('<div class="info-window">' + name + address + '</div>');
+          infoWindow.open(map, marker);
+          console.log(result);
+        });
+      });
+
+      function toggleBounce() {
+
+        if (marker.getAnimation() != null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
