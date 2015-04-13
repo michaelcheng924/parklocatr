46b19d01cbe956c9417cf8d40e0f367ff705f17a
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-12 22:34:42 -0500

    combine nearbysearch with photos and getdetails

--- client/scripts/models/park.js
+++ client/scripts/models/park.js
@@ -1,15 +1,4 @@
 var Park = Backbone.Model.extend({
   defaults: {
-    'Name': '',
-    'Address': '',
-    'CityMuni': 'Houston',
-    'County': '',
-    'State': 'TX',
-    'Zip': '',
-    'Park_Acres': '',
-    'Park_type': '',
-    'Flag': '',
-    'X_Coordinate': '',
-    'Y_Coordinate': ''
   }
 });
