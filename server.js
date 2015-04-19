5324340a7887cb430e466bf4a39f96d6c781caf4
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-19 14:16:23 -0500

    move server.js and add to passport setup

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ var app = express();
 
 app.use(express.static(__dirname + '/client'));
 
+// Configure passport
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 app.post('/login',
   passport.authenticate('local', {
     successRedirect: '/',
--- server/server.js
+++ server/server.js
@@ -1,8 +1,37 @@
 var express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
 var app = express();
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/../client'));
+
+// Configure passport
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+);
 
 var port = process.env.PORT || 3000;
 
