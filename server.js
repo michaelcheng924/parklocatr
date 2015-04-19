17cec836716de99329587d13b2027d2192847153
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-19 14:05:46 -0500

    add navbar, start authentication code

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-loca');
 
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+};
 
 var port = process.env.PORT || 3000;
 
