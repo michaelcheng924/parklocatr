99c75c7964114f3303cdb29955c7fd5d1576c695
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-21 21:53:02 -0500

    create login and signup templates

---
+++ client/scripts/templates/LoginTemplate.js
@@ -0,0 +1,18 @@
+<h2>Login</h2>
+<form action="/login" method="post">
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input id="username" type="text" name="username" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input id="password" type="password" name="password" class="form-control">
+    </div>
+    <div>
+      <input type="submit" value="Login" class="btn btn-primary">
+    </div>
+</form>
+
+<p>
+  <a href="/signup">Create an Account &rarr;</a>
+<p>
---
+++ client/scripts/templates/SignupTemplate.js
@@ -0,0 +1,18 @@
+<h2>Sign up</h2>
+<form action="/signup" method="post">
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input id="username" type="text" name="username" class="form-control">
+    </div>
+    <div class="form-group"> 
+      <label for="password">Password:</label>
+      <input id="password" type="password" name="password" class="form-control">
+    </div>
+    <div>
+      <input type="submit" value="Sign up" class="btn btn-primary">
+    </div>
+</form>
+
+<p>
+  <a href="/login">Login to your account &rarr;</a>
+<p>
