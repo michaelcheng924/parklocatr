e600c92b5a293d064fb467ebea5c48572c7679be
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-24 23:48:39 -0500

    move back handlesubmit to html form

--- client/scripts/templates/SignupViewTemplate.js
+++ client/scripts/templates/SignupViewTemplate.js
@@ -1,6 +1,6 @@
 <div class="container form-container">
   <h2>Sign up</h2>
-  <form class="signup-form" method="post">
+  <form action="/signup" class="signup-form" method="post">
       <div class="form-group">
         <label for="username">Username:</label>
         <input id="username" type="text" name="username" class="form-control username-signup">
--- client/scripts/views/SignupView.js
+++ client/scripts/views/SignupView.js
@@ -6,10 +6,6 @@ var SignupView = Backbone.View.extend({
     this.render();
   },
 
-  events: {
-    'submit .signup-form': 'handleSubmit'
-  },
-
   render: function() {
     $.ajax({
       url: 'scripts/templates/SignupViewTemplate.js',
@@ -18,21 +14,6 @@ var SignupView = Backbone.View.extend({
         var template = _.template(data, {});
         this.$el.html(template());
       }.bind(this)
-    });
-  },
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-
-    $.ajax({
-      type: 'POST',
-      url: '/signup',
-      data: {
-        username: $('.username-signup').val(),
-        password: $('.password-signup').val()
-      }
-    }).success(function(res) {
-      console.log('successfully posted!');
     });
   }
 });
