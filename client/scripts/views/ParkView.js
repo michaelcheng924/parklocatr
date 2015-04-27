82437af0ad22bfe28c15bf8471f234713b1ffe08
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-26 19:40:38 -0500

    refactor getParkDetails in parkView

--- client/scripts/views/ParkView.js
+++ client/scripts/views/ParkView.js
@@ -12,50 +12,53 @@ var ParkView = Backbone.View.extend({
   },
 
   events: {
-    'click': function() {
-      var service = new google.maps.places.PlacesService(map);
-
-      service.getDetails(this.model.attributes, function(result, status) {
-        if (status != google.maps.places.PlacesServiceStatus.OK) {
-          alert(status);
-          return;
-        }
-
-        var parkDetails = new ParkDetails(result);
-        var parkDetailsView = new ParkDetailsView({model: parkDetails});
-
-        infoWindow.close();
-
-        if (result.photos) {
-          var photos = result.photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35});
-        } else {
-          var photos = 'images/park-icon.png';
-        }
-
-        var marker = new google.maps.Marker({
-          map: map,
-          animation: google.maps.Animation.BOUNCE,
-          icon: photos,
-          position: result.geometry.location,
-        });
-
-        window.scrollTo(0,0);
-
-        setTimeout(function() {
-          marker.setAnimation(null);
-        }, 1400);
-
-        infoWindow.setContent('<div class="info-window">' + result.name + '</div>');
-        infoWindow.open(map, marker);
-
-        setTimeout(function() {
-          $('body').animate({scrollTop: $('.right-column').offset().top}, 500);
-        }, 1500);
-      });
-    }
+    'click': 'getParkDetails',
+    'click .want-to-visit': 'wantToVisit'
   },
 
   render: function() {
     return this.$el.html(this.template(this.model.attributes));
+  },
+
+  getParkDetails: function() {
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(this.model.attributes, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+
+      var parkDetails = new ParkDetails(result);
+      var parkDetailsView = new ParkDetailsView({model: parkDetails});
+
+      infoWindow.close();
+
+      if (result.photos) {
+        var photos = result.photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35});
+      } else {
+        var photos = 'images/park-icon.png';
+      }
+
+      var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.BOUNCE,
+        icon: photos,
+        position: result.geometry.location,
+      });
+
+      window.scrollTo(0,0);
+
+      setTimeout(function() {
+        marker.setAnimation(null);
+      }, 1400);
+
+      infoWindow.setContent('<div class="info-window">' + result.name + '</div>');
+      infoWindow.open(map, marker);
+
+      setTimeout(function() {
+        $('body').animate({scrollTop: $('.right-column').offset().top}, 500);
+      }, 1500);
+    });
   }
 });
