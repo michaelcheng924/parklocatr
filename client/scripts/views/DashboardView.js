3fef1f313b5879d0e08f61988a740e612d3683ac
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-27 22:34:42 -0500

    comment dashboardView

--- client/scripts/views/DashboardView.js
+++ client/scripts/views/DashboardView.js
@@ -17,34 +17,45 @@ var DashboardView = Backbone.View.extend({
 
   render: function() {
     var self = this;
+
+    // If no user is signed in, display dashboard features
     if (!localStorage.getItem('com.parklocatr')) {
+      // Get and display dashboard features template
       self.getTemplate('scripts/templates/PreDashboardViewTemplate.js', self);
+    // If a user is signed in, display personalized dashboard
     } else {
+      // Get and display personalized dashboard
       self.getTemplate('scripts/templates/DashboardViewTemplate.js', self)
       .then(function() {
+        // Get and display Parks to Visit
         self.getDashboardParks('/parks-to-visit', self, '.parks-to-visit-display')
       .then(function() {
+        // Get and display Visited Parks
         self.getDashboardParks('/visited-parks', self, '.visited-parks-display');
         });
       });
     }
   },
 
+  // Makes get request for template
   getTemplate: function(url, self) {
     return $.ajax({
       url: url,
       dataType: 'html',
       success: function(data) {
+        // Displays template in view
         self.displayTemplate(data, self);
       }
     });
   },
 
+  // Displays template in view
   displayTemplate: function(data, self) {
     var template = _.template(data, {});
     self.$el.html(template(self.model.attributes));
   },
 
+  // Makes get request for Dashboard parks lists
   getDashboardParks: function(url, self, className) {
     return $.ajax({
       url: url,
@@ -52,11 +63,13 @@ var DashboardView = Backbone.View.extend({
         'x-access-token': localStorage.getItem('com.parklocatr')
       },
       success: function(data) {
+        // Displays template in view
         self.displayDashboardParks(data, className);
       }
     });
   },
 
+  // Displays parks lists in view
   displayDashboardParks: function(data, className) {
     var display = $(className);
     display.html('');
